b1eb8c801f0ffef1dc4daf9d821cf714040875c3
Tran Tan Lam <trankind@gmail.com>
2016-05-21 17:52:45 +0700

    Handle textField

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'TipCalculator' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+	pod 'ChameleonFramework/Swift'
+	pod 'FlagKit', '~> 1.0'
+  # Pods for TipCalculator
+
+end
--- TipCalculator/Controllers/TipViewController.swift
+++ TipCalculator/Controllers/TipViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import ChameleonFramework
+import FlagKit
 
-class TipViewController: UIViewController {
+class TipViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var settingBarButton: UIBarButtonItem!
     @IBOutlet weak var amountView: UIView!
@@ -18,4 +20,79 @@ class TipViewController: UIViewController {
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
+    
+    var tip: Tip!
+    var currentTipPercent: Float {
+        get {
+            return tip.tipPercentage()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tip = .Low
+        
+        priceTextField.delegate = self
+        priceTextField.becomeFirstResponder()
+        priceTextField.addTarget(self,
+                                  action: #selector(TipViewController.priceTextFieldValueChanged(_:)),
+                                  forControlEvents: .EditingChanged)
+        
+        amountView.backgroundColor = UIColor.flatMintColor()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    // MARK: TextField Delegate method
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+    
+    func priceTextFieldValueChanged(sender: AnyObject) {
+        calculateTipAndTotalLabel()
+    }
+    
+    // MARK: Dismiss Keyboard
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: Update TipLabel And Total Label
+    func calculateTipAndTotalLabel() {
+        if priceTextField.text != "" {
+            tipLabel.text = "\(Float(priceTextField.text!)! * currentTipPercent)"
+            totalLabel.text = "\(Float(tipLabel.text!)! + (Float(priceTextField.text!))!)"
+        } else {
+            tipLabel.text = ""
+            totalLabel.text = ""
+        }
+    }
+    
+    // MARK: Segment Control Value change
+    
+    @IBAction func segmentControlAction(sender: UISegmentedControl) {
+        let currentSegmentIndex = sender.selectedSegmentIndex
+        switch currentSegmentIndex {
+        case 0 :
+            tip = Tip.Low
+            calculateTipAndTotalLabel()
+        case 1 :
+            tip = Tip.Medium
+            calculateTipAndTotalLabel()
+        case 2 :
+            tip = Tip.High
+            calculateTipAndTotalLabel()
+        default: break
+        }
+    }
+
+    @IBAction func settingButtonTapped(sender: UIBarButtonItem) {
+    }
 }
